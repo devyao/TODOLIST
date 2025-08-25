85be0e362b27ec91c1f504df80e6e2383886f164
zhangyao3 <zhangyao3@wps.cn>
2025-08-25 17:33:11 +0800

    fix: ecs5

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ const todoComputeData = computed(() => {
   <el-card class="box-card">
     <template #header>
       <div class="clearfix">
-        <span>待办List列表1</span>
+        <span>待办List列表111</span>
       </div>
     </template>
     <router-view></router-view>
