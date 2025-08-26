b7ff12f5077eac360f3841a6a9988d4a5f1bdf96
zhangyao3 <zhangyao3@wps.cn>
2025-08-26 11:13:06 +0800

    fix:cdnn

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    viteExternalsPlugin({
+      vue: 'Vue',
+      'vue-demi': 'VueDemi'
+    })
+  ]
 })
